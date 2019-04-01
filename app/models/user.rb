b5227b52fe9ae564d98ee524131455676903189d
Nathalie Santana <nathalie.scruz@gmail.com>
2019-04-01 04:55:34 +0000

    Define slug method.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ActiveRecord::Base
   has_many :custom_beverages
   has_secure_password
 
+  def slug
+    username.downcase.gsub(' ', '-')
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find {|s| s.slug == slug}
+  end
+
 end
