9230a2177c9ca07d21fb0385e781c74416e4e656
Nathalie Santana <nathalie.scruz@gmail.com>
2019-04-02 00:58:59 +0000

    Create get and post controller methods.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,12 @@
 class UsersController < ApplicationController
 
+  post '/signup' do
+    @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+    redirect'/custom_beverages' #a list of all users beverages.
+  end
+
+  get '/signup' do
+    erb :'signup'
+  end
+
 end
