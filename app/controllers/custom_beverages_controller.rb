780383dd615d47da91720d1acfcf460eb5081cc2
Nathalie Santana <nathalie.scruz@gmail.com>
2019-04-07 17:40:23 +0000

    User can access New Custom Bevegare form.

--- app/controllers/custom_beverages_controller.rb
+++ app/controllers/custom_beverages_controller.rb
@@ -7,7 +7,15 @@ class CustomBeverages < ApplicationController
     else
       redirect to '/login'
     end
+  end
 
+  get '/custombeverages/new' do
+    if !!logged_in?
+      @user = current_user
+      erb :'new_custom_beverage'
+    else
+      redirect to '/login'
+    end
   end
 
 end
