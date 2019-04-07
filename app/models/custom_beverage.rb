57c03b2168a10403555f37bd756d42b44e15b840
Nathalie Santana <nathalie.scruz@gmail.com>
2019-04-07 19:20:20 +0000

    Add slug method to customize beverage url

--- app/models/custom_beverage.rb
+++ app/models/custom_beverage.rb
@@ -1,3 +1,11 @@
 class CustomBeverage < ActiveRecord::Base
   belongs_to :user
+
+  def slug
+    title.downcase.gsub(' ', '-')
+  end
+
+  def self.find_by_slug(slug)
+    CustomBeverage.all.find {|s| s.slug == slug}
+  end
 end
